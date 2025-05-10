ba4de3c4bd8f41982e6a69b36cbe6bb8d517a8f3
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-05-11 00:05:25 +0530

    UsersModel Created

---
+++ server/models/usersModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+    },
+    mobile: {
+        type: String,
+        required: true,
+        match: [/^[6-9]\d{9}$/, 'Please enter a valid mobile number.']
+    },
+    profilePhoto: {
+        type: String
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    otp: {
+        type: String
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
